95c76ec9bc51673803354a75f963cfeae68437e5
vikassoni99 <vikassoni.soni22@gmail.com>
2020-07-27 10:53:04 +0530

    Added additional information in forecast

--- utils/forecast.js
+++ utils/forecast.js
@@ -4,11 +4,12 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.weatherbit.io/v2.0/current?&lat=' + latitude + '&lon=' + longitude + '&key=2686e69c11f34810a6155d7796436e6e'
     request({ url: url, json: true }, (error, response) => {
         // const raw= JSON.parse(response.body)
+        
         // console.log(raw.data[0].temp)
         if (error) {
             callback("Error connecting to weather services.", undefined)
         } else {
-            callback(undefined, response.body.data[0].temp)
+            callback(undefined, "Current Temperature is "+response.body.data[0].temp+" °C and" + " currently there are "+response.body.data[0].weather.description)
         }
     })
 }
